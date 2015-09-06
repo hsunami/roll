698b608eec013f4815fd72785d2eb538617dd1a0
Jeff Hsu <jeff@traintracks.io>
2015-09-06 21:02:03 +0800

    t2: (3) capitalize + decapitalize [3]

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,28 @@
 use std::char;
 
 mod roll {
+    pub fn capitalize(word: &str) -> String {
+        let capitalized = word.char_indices()
+            .flat_map(|(i, c)| if i == 0 { 
+                char::to_uppercase(c).next() 
+            } else { Some(c) })
+            .collect::<String>();
+
+        println!("{}", capitalized);
+        capitalized
+    }
+
+    pub fn decapitalize(word: &str) -> String {
+        let decapitalized = word.char_indices()
+            .flat_map(|(i, c)| if i == 0 { 
+                char::to_lowercase(c).next() 
+            } else { Some(c) })
+            .collect::<String>();
+
+        println!("{}", decapitalized);
+        decapitalized
+    }
+
     pub fn uppercase(word: &str) -> String {
         let uppercased = word.chars()
             .flat_map(char::to_uppercase)
@@ -15,9 +37,23 @@ mod tests {
     use roll;
 
     #[test]
+    fn test_capitalize_fn() {
+        let input = "hello world";
+        let output = "Hello world";
+        assert!(roll::capitalize(&input) == output);
+    }
+
+    #[test]
+    fn test_decapitalize_fn() {
+        let input = "HELLO WORLD";
+        let output = "hELLO WORLD";
+        assert!(roll::decapitalize(&input) == output);
+    }
+
+    #[test]
     fn test_uppercase_fn() {
-        let input = "hello_world";
-        let output = "HELLO_WORLD";
+        let input = "hello world";
+        let output = "HELLO WORLD";
         assert!(roll::uppercase(&input) == output);
     }
 }
